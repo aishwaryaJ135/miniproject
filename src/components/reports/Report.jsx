7c6028bd0fcf97dfd6f79ecb4c5bf52ec61bcf66
Jonathan Fidelis Paul <jonathan@dsquaresolutions.com>
2019-03-08 20:36:35 +0530

    Create stub for charts

--- src/components/reports/Report.jsx
+++ src/components/reports/Report.jsx
@@ -8,15 +8,44 @@ export class Report extends Component {
     prop: PropTypes
   };
 
+  //https://medium.com/@ridermansb/update-graph-in-d3-30baaebcc8c3
+  draw() {
+    const _this = this;
+    let data = _this.props.currSelectedItem.data;
+    var barHeight = 30;
+    d3.selectAll("svg > *").remove();
+
+    var bar = d3
+      .select("#miniSvg")
+      .selectAll(".rect")
+      .data(data);
+
+    //bar.exit().remove();
+
+    bar
+      .enter()
+      .append("rect")
+      .attr("class", "rect")
+      .attr("width", function(d) {
+        return d.ser;
+      })
+      .attr("height", barHeight - 1)
+      .attr("transform", function(d, i) {
+        return "translate(0," + i * barHeight + ")";
+      });
+  }
+
   render() {
     return (
       <div>
         <h2>This is the Report</h2>
         <svg id={"miniSvg"} />
+        {this.props.currSelectedItem.data ? this.draw() : null}
       </div>
     );
   }
 }
+
 const mapStateToProps = store => {
   let index = store.queries.findIndex(
     query => query.id === store.lastSelectedId
